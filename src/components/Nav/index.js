f9f312ac3e7f28b0294c71e14c8406218d0f04bd
Victor Nery <victornery.t@gmail.com>
2019-01-12 05:27:52 -0200

    Iniciando Nav

---
+++ src/components/Nav/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './nav.css'
+
+const Nav = () => (
+    <section className={styles.nav}>
+        <div className="container">
+        </div>
+    </section>
+)
+
+export default Nav
