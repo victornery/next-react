eaf51868a0c3571b1efca121666d749cdf40ee23
Victor Nery <victornery.t@gmail.com>
2019-01-12 05:27:33 -0200

    Iniciando seção Prelude

---
+++ src/components/Prelude/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './prelude.css'
+
+const Prelude = () => (
+    <section className={styles.prelude}>
+        <div className="container">
+        </div>
+    </section>
+)
+
+export default Prelude
