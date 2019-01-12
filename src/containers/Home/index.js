1b957c53971764668bfd80a2e18d2a3dea078c09
Victor Nery <victornery.t@gmail.com>
2019-01-12 05:28:25 -0200

    Arquivo container da Home

---
+++ src/containers/Home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Header from '../../components/Header'
+
+class Home extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return <Header />
+    }
+}
+
+export default Home
