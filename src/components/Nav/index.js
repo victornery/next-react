01a99a26586346edaa6ea795bdcadb9502e93a81
Victor Nery <victornery.t@gmail.com>
2019-01-13 22:16:32 -0200

    Estrutura inicial do componente Nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,10 +2,16 @@ import React from 'react'
 import styles from './nav.css'
 
 const Nav = () => (
-    <section className={styles.nav}>
-        <div className="container">
-        </div>
-    </section>
+    <nav className={styles.nav}>
+        <ul className={ styles.nav__list }>
+            <li>next</li>
+            <li>Flow</li>
+            <li>Vaquinha</li>
+            <li>Objetivos</li>
+            <li>Mimos</li>
+            <li>Propostas</li>
+        </ul>
+    </nav>
 )
 
 export default Nav
