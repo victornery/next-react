6f0dc093f45246c87e5914d98de684bfb29b8529
Victor Nery <victornery.t@gmail.com>
2019-01-13 22:16:42 -0200

    Estrutura inicial da seção Prelude

--- src/components/Prelude/index.js
+++ src/components/Prelude/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import styles from './prelude.css'
+import cover from './cover-image.png'
 
 const Prelude = () => (
     <section className={styles.prelude}>
         <div className="container">
+            <div className={ styles.prelude__content }>
+                <h2 className={ styles.prelude__title }>Pra ganhar descontos exclusivo, vem pro next</h2>
+                <p className={ styles.prelude__about }>Não é programa de ponto. É só nosso jeitinho de te mimar.</p>
+            </div>
+            <figure className={ styles.prelude__cover }>
+                <img alt="Vem pro next" src={ cover } />
+            </figure>
         </div>
     </section>
 )
