d78e1a62fa4daa462731e9c62d1810300d5a64e7
Victor Nery <victornery.t@gmail.com>
2019-01-13 22:17:54 -0200

    Estrutura inicial do Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import Nav from 'components/Nav'
+import { Logo } from '../../utils/icons'
 import styles from './header.css'
 
-const Header = () => <header className={ styles.header }>Oi, eu sou um Header!</header>
+const Header = () => (
+    <header className={ styles.header }>
+        <Logo />
+        <Nav />
+        <button>Abra sua conta</button>
+    </header>
+)
 
 export default Header
