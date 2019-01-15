7432677ea8c5084a03d6e11bd1df66c018ae8fac
Victor Nery <victornery.t@gmail.com>
2019-01-15 17:57:31 -0300

    Importando componente Cover nas seções

--- src/components/Prelude/index.js
+++ src/components/Prelude/index.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import styles from './prelude.css'
 import cover from './cover-image.png'
+import Cover from 'components/Cover'
 
 const Prelude = () => (
-    <section className={styles.prelude}>
-        <div className="container container-flex">
-            <div className={ styles.prelude__content }>
-                <h2 className={ styles.prelude__title }>Pra ganhar descontos exclusivos, vem pro next</h2>
-                <p className={ styles.prelude__about }>Não é programa de ponto. <wbr /> É só nosso jeitinho de te mimar {`<3`}</p>
-            </div>
-            <figure className={ styles.prelude__cover }>
-                <img alt="Vem pro next" src={ cover } />
-            </figure>
-        </div>
-    </section>
+   <section className={styles.prelude}>
+      <div className="container container-flex">
+         <div className={styles.prelude__content}>
+            <h2 className={styles.prelude__title}>Pra ganhar descontos exclusivos, vem pro next</h2>
+            <p className={styles.prelude__about}>Não é programa de ponto. <wbr /> É só nosso jeitinho de te mimar {`<3`}</p>
+         </div>
+         <Cover image={cover} alt="Vem pro Next! ;-)" />
+      </div>
+   </section>
 )
 
 export default Prelude
--- src/components/Saving/index.js
+++ src/components/Saving/index.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import styles from './saving.css'
 import SavingItem from 'components/SavingItem'
+import Cover from 'components/Cover'
 
 const Saving = () => (
-    <section className={styles.saving}>
-        <div className='container'>
-            <h2 className={ styles.saving__title }>É só ter conta no next que sobra $ de verdade</h2>
+   <section className={styles.saving}>
+      <div className='container'>
+         <h2 className={styles.saving__title}>É só ter conta no next que sobra $ de verdade</h2>
 
-            <ul className={ styles.saving__brands }>
+         <ul className={styles.saving__brands}>
 
-                {/* <SavingItem icon={} offer={} description={} /> */}
+            {/* <SavingItem icon={} offer={} description={} /> */}
 
-            </ul>
-        </div>
-    </section>
+         </ul>
+
+         <Cover alt="Sobra grana mesmo. É só ter conta no next. Ainda dá tempo." />
+
+      </div>
+   </section>
 )
 
 export default Saving
