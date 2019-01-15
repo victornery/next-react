76e08f1365d45cdf439968d91ce0b3430a49ee82
Victor Nery <victornery.t@gmail.com>
2019-01-15 05:17:18 -0200

    Atualizando Header - inserindo consumer da context

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,25 @@
 import React from 'react'
-import Nav from 'components/Nav'
-import { Logo } from '../../utils/icons'
 import styles from './header.css'
+import Nav from 'components/Nav'
+import Icons from 'components/Icons'
+import Button from 'components/Button'
+import AppContext from '../../utils/context'
 
-const Header = () => (
-    <header className={ styles.header }>
-        <Logo />
-        <Nav />
-        <button>Abra sua conta</button>
-    </header>
-)
+const Header = () => {
+    return (
+        <AppContext.Consumer>
+            { context => (
+                <header className={ styles.header }>
+                    <div className="container container-flex">
+                        <Icons name="logo" style={{height: '50px'}} />
+                        <Nav items={ context.nav } />
+                        <Button title="Abra sua conta" />
+                    </div>
+                </header>
+                )
+            }
+        </AppContext.Consumer>
+    )
+}
 
 export default Header
