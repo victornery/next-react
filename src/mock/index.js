7df2eeb5a5640f3dae98d252fdeedd4e060c4d6c
Victor Nery <victornery.t@gmail.com>
2019-01-15 05:09:44 -0200

    Criando novas rotas pro mock

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,4 +1,9 @@
+/*
+**  Criando servidor Express para servir os mock
+*/
+
 const express = require('express') //   Importando Express
+const cors = require('cors')
 const app = express() //    Criando instância do Express
 const port = 8000 //    Setando porta para servir sv express
 
@@ -18,10 +23,18 @@ app.get('/api/gifts/', (req, res) => {
     res.sendFile(`${__dirname}/api/gift.json`)
 })
 
+app.get('/api/nav/', (req, res) => {
+    res.sendFile(`${__dirname}/api/navigation.json`)
+})
+
+app.get('/api/savings/', (req, res) => {
+    res.sendFile(`${__dirname}/api/savings.json`)
+})
+
 /*
 **  Aqui, podemos substituir os endpoints somente para o express
-**  servir arquivos estáticos. Fica aí um ponto legal para se
-**  pensar. 🧐
+**  servir arquivos estáticos. Fica aí um ponto para se refletir
+**  🧐
 **
 **  app.use(express.static(__dirname))
 */
