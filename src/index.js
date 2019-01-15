18a3fc6d3ed73a4b6d029ac7d72e0f06744b398f
Victor Nery <victornery.t@gmail.com>
2019-01-15 05:15:02 -0200

    app.js - criando async/await + promise de fetchs + context

--- src/index.js
+++ src/index.js
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import Home from './containers/Home'
+import AppContext from './utils/context'
 import '../public/css/all.css' // CSS Commons
 
-const App = () => <Home />
+class App extends Component {
+    state = {
+        gifts: [],
+        nav: [],
+        savings: []
+    }
+
+    componentDidMount() {
+
+        const url = [
+            'http://127.0.0.1:8080/api/gifts',
+            'http://127.0.0.1:8080/api/nav',
+            'http://127.0.0.1:8080/api/savings'
+        ]
+        
+        const handleFetchs = async () => 
+            await Promise.all(url.map(url => 
+                fetch(url)
+                    .then(data => data.json())
+            ))
+            .then(data => {
+                const [gifts, nav, savings] = data;
+
+                this.setState({ gifts })
+                this.setState({ nav })
+                this.setState({ savings })
+            })
+            
+        handleFetchs()
+    }
+
+    render() {
+        const { gifts, nav, savings } = this.state
+        return (
+            <AppContext.Provider value={{
+                gifts,
+                nav,
+                savings
+            }}>
+                <Home />
+            </AppContext.Provider>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById("next"))
