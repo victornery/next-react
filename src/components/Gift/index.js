52abeb8d0939070da9416712e8fbca8bc1c93a50
Victor Nery <victornery.t@gmail.com>
2019-01-16 08:12:29 -0200

    Preparando componente Gift para receber Context API

--- src/components/Gift/index.js
+++ src/components/Gift/index.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react'
 import styles from './gift.css'
-import AppContext from '../../utils/context'
+import AppContext from 'utils/context'
 import GiftItem from 'components/GiftItem'
+import Modality from 'components/Modality'
 
 class Gift extends Component {
    constructor(props) {
       super(props)
 
       this.state = {
-         isCredit: true
+         modalOpen: false,
+         modality: 'credit',
+         typeGift: 'alimentacao'
       }
    }
 
-   render() {
-      const { benefits } = this.props
-      const { isCredit } = this.state
+   handleModal = (node) => {
+      e.preventDefault();
+      console.log('Testando')
+   }
+
+   changeModality = (node) => {
+      const { modality } = this.state
+      const clicked = node.target.dataset.modality
+
+      clicked && 
+         clicked !== modality && this.setState({ modality: clicked })
+   }
+
+   handleBenefits = (benefits) => {
+      console.log(benefits)
+
+      return <GiftItem icon={} />
+   }
+
+   render () {
 
       return (
          <section className={styles.gift}>
@@ -24,22 +44,19 @@ class Gift extends Component {
                      <h2 className={styles.gift__title}>Apresentamos os Mimos</h2>
                      <p className={styles.gift__description}>Parceiros exclusivos com desconto só pra vc, que é next.</p>
                   </div>
-                  <a className={styles.gift__rules} href="#">Regras de Uso</a>
+
+                  <a onClick={ this.handleModal } className={styles.gift__rules} href="#">Regras de Uso</a>
 
                   <ul className={styles.gift__choose}>
                      <AppContext.Consumer>
-                        {context &&
-                           <GiftItem key={context.gifts.modality} name={context.gifts.title} />
+                        {context => context.gifts.map((type) => (
+                           <Modality onClick={this.changeModality} onLoad={this.handleBenefits(type.typeGifts)} checked={ this.state.modality === type.modality ? true : false } name={type.title} category={type.modality} key={type.modality} />
+                        ))
                         }
                      </AppContext.Consumer>
                   </ul>
 
                   <ul className={styles.gift__benefits}>
-                     {benefits &&
-                        benefits.map((item) => {
-                           <GiftItem name={item.title} icon={item.icon} />
-                        })
-                     }
                   </ul>
                </div>
             </div>
