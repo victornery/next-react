554bee66e1130a21844c3b34a805d901ead9b6c2
Victor Nery <victornery.t@gmail.com>
2019-01-18 07:22:26 -0200

    iniciando loop no saving

--- src/components/Saving/index.js
+++ src/components/Saving/index.js
@@ -4,12 +4,20 @@ import SavingItem from 'components/SavingItem'
 import Cover from 'components/Cover'
 import cover from './card-hand.png'
 
+import AppContext from 'utils/context'
+
 const Saving = () => (
    <section className={styles.saving}>
       <div className='container'>
          <h2 className={styles.saving__title}>É só ter conta no next que sobra $ de verdade</h2>
 
          <ul className={styles.saving__brands}>
+
+            <AppContext.Consumer>
+                { context => console.log(context)
+                
+                }
+            </AppContext.Consumer>
 
             {/* <SavingItem icon={} offer={} description={} /> */}
 
