99a5e47f105eae68ac8e07ff9b203d7f06a7b5f4
Victor Nery <victornery.t@gmail.com>
2019-01-18 07:22:07 -0200

    Inserindo data attribute e ajuste css no componente Giftitem

--- src/components/GiftItem/index.js
+++ src/components/GiftItem/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import styles from './item.css'
 
-const GiftItem = ({ name, icon }) => (
-    <li className={ styles.item } key={icon ? `${ name }-${ icon }` : name}>
+const GiftItem = ({ name, icon, onClick }) => (
+    <li onClick={onClick} className={ styles.item } key={icon ? `${ name }-${ icon }` : name} data-gift={name}>
         { icon }
         <span className={ styles.title }>{ name }</span>
     </li>
